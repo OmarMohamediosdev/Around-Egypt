6fff1695d10f41aca913fca41c461f3d010b6118
OmarMohamediosdev <45975191+OmarMohamediosdev@users.noreply.github.com>
2025-10-13 22:22:25 +0300

    initial commit

---
+++ Around Egypt/Around Egypt/Around_EgyptApp.swift
@@ -0,0 +1,17 @@
+//
+//  Around_EgyptApp.swift
+//  Around Egypt
+//
+//  Created by Omar Mohamed on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Around_EgyptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
